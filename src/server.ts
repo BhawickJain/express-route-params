da7f837b0ead1cdebe013a6a15bb5d7488274d06
Bhawick Jain <bhawick@gmail.com>
2022-08-08 16:42:39 +0100

    added notes on types, found a way to iterate over multiple path parameters

--- src/server.ts
+++ src/server.ts
@@ -8,47 +8,88 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/eat/apple", (req, res) => {
+app.get<{ food: string }>("/eat/:food", (req, res) => {
+  const food = req.params.food;
+  const foodProper: string = "aeiou".includes(food[0])
+    ? `an ${food}`
+    : `a ${food}`;
   res.json({
-    message: "Yum yum - you ate an apple!",
+    message: `Yum yum - you ate ${foodProper}`,
   });
 });
 
-app.get("/eat/banana", (req, res) => {
+// app.get("/eat/apple", (req, res) => {
+//   res.json({
+//     message: "Yum yum - you ate an apple!",
+//   });
+// });
+
+// app.get("/eat/banana", (req, res) => {
+//   res.json({
+//     message: "Yum yum - you ate a banana!",
+//   });
+// });
+
+// app.get("/eat/carrot", (req, res) => {
+//   res.json({
+//     message: "Yum yum - you ate a carrot!",
+//   });
+// });
+
+app.get<{ exampleRouteParameter: string }>(
+  "/echo/:exampleRouteParameter",
+  (req, res) => {
+    const echoContent = req.params.exampleRouteParameter;
+    res.json({
+      echo: echoContent,
+      message: `I am echoing back to you: ${echoContent}`,
+    });
+  }
+);
+
+app.get<{ something: string }>("/shout/:something", (req, res) => {
   res.json({
-    message: "Yum yum - you ate a banana!",
+    shout: `${req.params.something.toUpperCase()}!`,
+    result: `I am shouting back to you: ${req.params.something.toUpperCase()}!`,
   });
 });
 
-app.get("/eat/carrot", (req, res) => {
-  res.json({
-    message: "Yum yum - you ate a carrot!",
-  });
-});
+app.get<{ numOne: string; numTwo: string; numThree: string }>(
+  "/add/:numOne/:numTwo/:numThree?",
+  (req, res) => {
+    const filterOutEmpty: [string, string][] = Object.entries(
+      req.params
+    ).filter((el) => el[1] !== undefined);
+    const arrayOfNum: number[] = filterOutEmpty.map((el) => parseInt(el[1]));
+    const addition = arrayOfNum.reduce((prev, curr) => prev + curr);
+    const stringAddition: string = arrayOfNum
+      .map((num) => num.toString())
+      .join(" + ");
+    res.json({
+      original: stringAddition,
+      result: addition,
+    });
+  }
+);
 
-app.get("/echo/:exampleRouteParameter", (req, res) => {
-  const echoContent = req.params.exampleRouteParameter;
-  res.json({
-    echo: echoContent,
-    message: `I am echoing back to you: ${echoContent}`,
-  });
-});
-
-app.get("/multiply/:numOne/:numTwo", (req, res) => {
-  /**
-   * Note that `numOne` and `numTwo` are both typed as string.
-   * (Hover over with your mouse to see!)
-   *
-   * Route params are, by default, typed as strings when they
-   * are parsed by Express.
-   */
-  const { numOne, numTwo } = req.params;
-  const multiplication = parseInt(numOne) * parseInt(numTwo);
-  res.json({
-    original: `${numOne} x ${numTwo}`,
-    result: multiplication,
-  });
-});
+app.get<{ numOne: string; numTwo: string }>(
+  "/multiply/:numOne/:numTwo",
+  (req, res) => {
+    /**
+     * Note that `numOne` and `numTwo` are both typed as string.
+     * (Hover over with your mouse to see!)
+     *
+     * Route params are, by default, typed as strings when they
+     * are parsed by Express.
+     */
+    const { numOne, numTwo } = req.params;
+    const multiplication = parseInt(numOne) * parseInt(numTwo);
+    res.json({
+      original: `${numOne} x ${numTwo}`,
+      result: multiplication,
+    });
+  }
+);
 
 /**
  * `app.get` can take a type argument.
@@ -56,6 +97,11 @@ app.get("/multiply/:numOne/:numTwo", (req, res) => {
  *  This could be the name of an existing type (e.g. an interface)
  *    or a literal object type that is provided directly, as below.
  */
+///// !! declaring param with type enforces its correct use
+///// name in curly braces must match that of the path
+///// gives type and autocomplete hints as well within the body
+///// type must be string as the transpiled javascript will see
+///// it that way and you need to type cast it.
 app.get<{ name: string }>("/happy-birthday/:name", (req, res) => {
   res.json({
     lyrics: [
